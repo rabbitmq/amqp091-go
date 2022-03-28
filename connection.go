3d7f2f9ffa267601d3dab65ac5373fe90c93ea0f
Daniele Palaia <dpalaia@dpalaia-a02.vmware.com>
2022-03-28 09:48:12 +0200

    bump versioning to 1.3.1

--- connection.go
+++ connection.go
@@ -24,7 +24,7 @@ const (
 	defaultHeartbeat         = 10 * time.Second
 	defaultConnectionTimeout = 30 * time.Second
 	defaultProduct           = "Amqp 0.9.1 Client"
-	buildVersion             = "1.3.0"
+	buildVersion             = "1.3.1"
 	platform                 = "golang"
 	// Safer default that makes channel leaks a lot easier to spot
 	// before they create operational headaches. See https://github.com/rabbitmq/rabbitmq-server/issues/1593.
